5af5156398dbd8088c2e7308c3664b1c26c72112
Ian Chen <ianre657@gmail.com>
2018-04-16 00:29:48 +0800

    warp by class

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ import requests
 from pprint import pprint
 
 url ='https://timetable.nctu.edu.tw/'
-para={ "r":"main/get_cos_list"}
 
-form_keys= ["m_acy","m_sem","m_degree","m_dep_id","m_group","m_grade","m_class","m_option","m_crsname","m_teaname","m_cos_id","m_cos_code","m_crstime","m_crsoutline","m_costype"]
+class query:
+  def __init__(self,kwargs):
+    '''
+    acy : academic year
+    sem : semester
+    degree:
+    dep_id: department id
+    group:
+    grade:
+    class:
+    '''
+    self.play_load=dict()
+    form_keys= ["m_acy","m_sem","m_degree","m_dep_id","m_group","m_grade",
+    "m_class","m_option","m_crsname","m_teaname","m_cos_id","m_cos_code",
+    "m_crstime","m_crsoutline","m_costype"]
+    for i in form_keys:
+      self.play_load[i] = "**"
+    if len(kwargs) != 0:
+      for k,v in kwargs.items():
+        if k in self.play_load.keys():
+          self.play_load[k] = v
 
-play_load={}
-for i in form_keys:
-  play_load[i] = "**"
+  def expand(self):
+    pass
 
-play_load['m_acy'] = 106
-play_load['m_sem'] = 2
-play_load['m_degree'] = 3
-play_load['m_dep_id'] = 43
+  def start(self):
+    para={ "r":"main/get_cos_list"}
+    return requests.post(url,params=para,data=self.play_load).json()
 
-r = requests.post(url,params=para,data=play_load)
-pprint(r.json())
+def main():
+  q = query({"m_acy":106, "m_sem":2, "m_degree":0,"m_dep_id":'U9' })
+  pprint(q.start())
+
+if __name__ == "__main__":
+  main()
